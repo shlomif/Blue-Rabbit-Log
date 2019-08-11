4bb837e801b4f02b913842b899d84894700e24a6
Shlomi Fish <shlomif@shlomifish.org>
2019-08-12 01:33:26 +0300

    avoid recursive make

--- Blue-Rabbit-Log/screenplay/scripts/prepare-epub.pl
+++ Blue-Rabbit-Log/screenplay/scripts/prepare-epub.pl
@@ -5,49 +5,50 @@ use warnings;
 
 use utf8;
 
-use Shlomif::Screenplays::EPUB;
+use Shlomif::Screenplays::EPUB ();
+
+use Path::Tiny qw/ path /;
 
 my $obj = Shlomif::Screenplays::EPUB->new;
 $obj->run;
 
-my $gfx = $obj->gfx;
+my $gfx      = $obj->gfx;
 my $filename = $obj->filename;
 
 my $base = 'Blue-Rabbit-Log-part-';
-foreach my $part ($filename =~ /\A\Q$base\E([0-9]+)/g)
+foreach my $part ( path($filename)->basename =~ /\A\Q$base\E([0-9]+)/g )
 {
-    my $epub_basename = $base.$part;
+    my $epub_basename = $base . $part;
     $obj->epub_basename($epub_basename);
 
     $obj->output_json(
         {
-            data =>
-            {
+            data => {
                 filename => $epub_basename,
-                title => qq/The Blue Rabbit Log - $part/,
-                authors =>
-                [
+                title    => qq/The Blue Rabbit Log - $part/,
+                authors  => [
                     {
                         name => "Shlomi Fish",
                         sort => "Fish, Shlomi",
                     },
                 ],
-                contributors =>
-                [
+                contributors => [
                     {
                         name => "Shlomi Fish",
                         role => "oth",
                     },
                 ],
                 cover => "images/$gfx",
-                rights => "Creative Commons Attribution Noncommercial ShareAlike Unported (CC-by-nc-sa-3.0)",
+                rights =>
+"Creative Commons Attribution Noncommercial ShareAlike Unported (CC-by-nc-sa-3.0)",
                 publisher => 'http://www.shlomifish.org/',
-                language => 'en-GB',
-                subjects => [ 'FICTION/Horror', 'FICTION/Humorous', 'FICTION/Mashups', ],
-                identifier =>
-                {
+                language  => 'en-GB',
+                subjects  => [
+                    'FICTION/Horror', 'FICTION/Humorous', 'FICTION/Mashups',
+                ],
+                identifier => {
                     scheme => 'URL',
-                    value => 'http://www.shlomifish.org/humour/TOWTF/',
+                    value  => 'http://www.shlomifish.org/humour/TOWTF/',
                 },
             },
         },
